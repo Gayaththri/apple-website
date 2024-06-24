9dbf198953c6623ba1fecf8f7fba51ba54ca5164
Gayaththri <sriigaya3@gmail.com>
2024-06-24 13:05:40 +0530

    Added Navbar, Hero, and Highlights components with initial structure and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 };
 export default App;
