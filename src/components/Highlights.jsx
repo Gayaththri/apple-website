d306126b2a5bf1db03d864f897acb5cd963f858c
Gayaththri <sriigaya3@gmail.com>
2024-06-25 13:40:25 +0530

    Added GSAP animations to React components and integrate Three.js for 3D model views for the iPhone model

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,13 +1,10 @@
-{
-  /*Highlights page of the apple website */
-}
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
+
 import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
-  // useGSAP hook to trigger GSAP animations
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0 });
     gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
@@ -23,6 +20,7 @@ const Highlights = () => {
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
+
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
               Watch the film
